6a1a381df1b0c462c9392cf8ef0e7a7b134a231c
baole <bao16122001@gmail.com>
2024-10-23 09:20:48 +0700

    commit

--- app/src/main/java/com/example/movieapp/ui/RegisterActivity.java
+++ app/src/main/java/com/example/movieapp/ui/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.movieapp.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +11,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.movieapp.R;
+import com.example.movieapp.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        getWindow().setStatusBarColor(getResources().getColor(android.R.color.black));
+        navigateToLoginActivity();
 
     }
+
+    private void navigateToLoginActivity() {
+        binding.signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
 }
